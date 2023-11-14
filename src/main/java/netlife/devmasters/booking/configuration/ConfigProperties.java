82c242c3f642c7bfa8421f188714f5349be677ea
JG <ludwing.gomez@epn.edu.ec>
2023-11-13 22:00:39 -0500

    feat: configuration extras

--- src/main/java/netlife/devmasters/booking/configuration/ConfigProperties.java
+++ src/main/java/netlife/devmasters/booking/configuration/ConfigProperties.java
@@ -3,11 +3,16 @@ package netlife.devmasters.booking.configuration;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class ConfigProperties {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
