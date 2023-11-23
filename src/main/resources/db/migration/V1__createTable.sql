66074eb2864b8c0e4605cced4730fe9e80e8bd37
JG <ludwing.gomez@epn.edu.ec>
2023-11-23 15:50:37 -0500

    feature: flyway works

---
+++ src/main/resources/db/migration/V1__createTable.sql
@@ -0,0 +1,6 @@
+CREATE TABLE location_prueba (
+                                 id_location_prueba SERIAL NOT NULL,
+                                 floor INTEGER NULL,
+                                 address VARCHAR(250) NULL,
+                                 place VARCHAR(250) NULL
+);
